16bb38e4535cf981ed5988e52afe3c4c70a68c1c
unknown <divya.ramesh@DIRA-BLR-01.accolite.com>
2018-01-14 19:12:17 +0530

    Assignments AU-2

--- SOAP Assignment/AssignmentPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.au.soapAssignment;
-import javax.xml.ws.Endpoint;
-
-public class AssignmentPublisher {
-	public static void main(String[] args) {
-		  Endpoint.publish("http://localhost:8080/WS/Assignment",new AssignmentImpl());
-		 }
-}
