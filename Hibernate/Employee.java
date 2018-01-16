60b45fedf73ab906a11cc194446575517cc702a7
unknown <divya.ramesh@DIRA-BLR-01.accolite.com>
2018-01-16 09:47:26 +0530

    AU assignment

---
+++ Hibernate/Employee.java
@@ -0,0 +1,55 @@
+package com.AU.Assignment;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Employee {
+	@Id
+	private int eid;
+	private String name;
+	private int salary;
+	
+	@OneToOne
+	private Laptop laptop;
+	
+	@OneToMany
+	private List<Expenses> expenses;
+	
+	public List<Expenses> getExpenses() {
+		return expenses;
+	}
+	public void setExpenses(List<Expenses> expenses) {
+		this.expenses = expenses;
+	}
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+}
---
+++ Hibernate/Laptop.java
@@ -0,0 +1,41 @@
+package com.AU.Assignment;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Laptop {
+	@Id
+	private int lid;
+	private String brand;
+	private int price;
+	
+	@OneToOne
+	private Employee employee;
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
